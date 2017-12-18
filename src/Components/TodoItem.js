d14993ae273a94096ac49a0424fd5460835fef1c
joe.wu <traveller_wjoe@sina.com>
2017-12-19 01:51:56 +0800

    update TodoItem classes

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,37 +1,46 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { FormControlLabel, Checkbox } from 'material-ui'
+import withStyles from 'material-ui/styles/withStyles';
 
-export default class ToDoItem extends Component {
+const styles = {
+    todoItem: {
+        width: '100%'
+    }
+}
+
+class ToDoItem extends Component {
     static propTypes = {
         // prop: PropTypes
-        children:PropTypes.string.isRequired
+        children: PropTypes.string.isRequired
     }
     state = {
         checked: false
     }
-    handleChange = () => {
+    handleChange = (e) => {
+        console.log(e.currentTarget)
         const checked = !this.state.checked
         this.setState({
             checked
         })
     }
     render() {
-        const { children } = this.props
+        const { children, classes } = this.props
         const { checked } = this.state
         return (
-            <section className="todo-item">
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            checked={checked}
-                            onChange={this.handleChange}
-                            value="1"
-                        />
-                    }
-                    label={children}
-                />
-            </section>
+            <FormControlLabel
+                className={classes.todoItem}
+                control={
+                    <Checkbox
+                        checked={checked}
+                        onChange={this.handleChange}
+                        value="1"
+                    />
+                }
+                label={children}
+            />
         )
     }
 }
+
+export default withStyles(styles)(ToDoItem)
